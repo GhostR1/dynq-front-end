79a7dcbc5cecdae50f430eaa7e83e306f4633dd0
GhostR <nataliia.semenova@nure.ua>
2020-12-01 07:41:44 +0200

    Admin is Done. Employee page remain. Localization remain.

--- src/components/NavbarAdmin.vue
+++ src/components/NavbarAdmin.vue
@@ -13,7 +13,6 @@
           <b-nav-item v-on:click="move_to_problem" right>Problems</b-nav-item>
         </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown text="Language" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
